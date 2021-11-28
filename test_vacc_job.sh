a0109e6e86de4bd3f79b14ab7f761747074e50e1
erikweis <32458904+erikweis@users.noreply.github.com>
2021-11-27 22:12:56 -0500

    update test_vacc

--- test_vacc_job.sh
+++ test_vacc_job.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #SBATCH --partition=short
 #SBATCH --nodes=1
-#SBATCH --mem=8gb
+#SBATCH --mem=1gb
 #SBATCH --time=02:59:59
 #SBATCH --job-name=1997
 
-python discretebranchingprocess.py --cutoff 20 -f test.csv
+python discretebranchingprocess.py -f test2.csv
