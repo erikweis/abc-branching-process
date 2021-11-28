b08879bfb6363f1997642a3b6c8119cb0c4c3361
erikweis <32458904+erikweis@users.noreply.github.com>
2021-11-28 17:56:50 -0500

    new priors

--- analyze.py
+++ analyze.py
@@ -3,6 +3,7 @@ from datetime import datetime
 import os
 import seaborn as sns
 import matplotlib.pyplot as plt
+import json
 
 class ABCAnalysis:
 
@@ -14,6 +15,16 @@ class ABCAnalysis:
 
         self.df['trial_success'] = self.df.apply(self.determine_trial_success,axis=1)
 
+        #load params
+        with open(os.path.join(self.dirpath,'hyperparams.json')) as f:
+            for k,v in json.load(f).items():
+                self.__setattr__(k,v)
+
+        #hard code state for now
+        self.state = 'vt'
+        data_path = os.path.join('data',f'{self.state}_first_peak.csv')
+        self.cumulative_cases_data = pd.read_csv(data_path).values
+
     def number_successful_trials(self):
         return sum(self.df['trial_success'])
 
@@ -21,13 +32,15 @@ class ABCAnalysis:
     def determine_trial_success(self,row):
 
         trial_ID = int(row['Unnamed: 0'])
-
         path = os.path.join(self.dirpath,f'trial_{trial_ID}.csv')
 
         with open(path,'r') as f:
-            if f.read().startswith('Failure'):
+            text = f.read()
+            if text.startswith('Failure'):
+                print(text)
                 return 0
-            return 1
+            else:
+                return 1
 
 
     def pairplot_R0_k(self,successful_trials_only = True):
@@ -45,10 +58,25 @@ class ABCAnalysis:
     def plot_priors(self):
         pass
 
+    def plot_results(self):
+
+        for index, row in self.df.iterrows():
+            if row['trial_success']:
+
+                path = os.path.join(self.dirpath,f'trial_{index}.csv')
+                temp_df = pd.read_csv(path)
+                vals = temp_df['cumulative_cases_simulated'].values
+                plt.plot(vals,color='blue',alpha=0.1)
+
+        plt.plot(self.cumulative_cases_data,color='red',linewidth = 2)
+        plt.show()
+
+
 if __name__ == "__main__":
     
     #specify a foldername
-    foldername = '11-28_15-39-40'
+    #foldername = '11-28_16-13-31' #100 with wide prior tight confidence (0.5)
+    foldername = '11-28_15-39-40' #1000 with wide priors
 
     #if no foldername specified, use the most recent dated folder
     if not foldername:
@@ -60,5 +88,6 @@ if __name__ == "__main__":
     abca = ABCAnalysis(foldername)
 
     #make pairplot
-    print(abca.number_successful_trials())
-    abca.pairplot_R0_k()
+    #print(abca.number_successful_trials())
+    abca.plot_results()
+    #abca.pairplot_R0_k()
