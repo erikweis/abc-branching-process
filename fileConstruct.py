78fd26ea63a29303b68973f5c6b7642b796b7ba9
mariahcboudreau <mariah.boudreau@uvm.edu>
2021-11-27 22:57:36 -0500

    Added fileConstruct

---
+++ fileConstruct.py
@@ -0,0 +1,77 @@
+import os
+import json
+import sys
+import pandas as pd
+import argparse 
+
+from priors import priors
+
+class WriteFiles:
+    """Class to write the files for the trials and parameters.
+    """
+
+    def __init__(
+            self,
+            state='vt',
+            foldername=None,
+            cutoff_time=90,
+            A_R0=1,
+            B_R0=4,
+            A_k=.1,
+            B_k=.5,
+            error=1, 
+            trials=100):
+        self.A_R0 = A_R0
+        self.B_R0 = B_R0
+        self.A_k = A_k
+        self.B_k = B_k
+        self.error = error
+        self.cutoff_time = cutoff_time
+        #self.cumulative_cases_data = pd.read_csv(f'data/{state}_first_peak.csv').values
+
+
+
+        dirname = datetime.now().strftime("%m-%d_%H-%M-%S") if foldername is None else foldername
+        self.dirpath = os.path.join('simulations', dirname)
+        os.mkdir(self.dirpath)
+
+        # save hyperparams to a json file
+        hyperparams = {'A_R0': A_R0, 'B_R0': B_R0, 'A_k': A_k, 'B_k': B_k}
+        json_fpath = os.path.join(self.dirpath, 'hyperparams.json')
+        with open(json_fpath, 'w') as f:
+            json.dump(hyperparams, f)
+
+        #pull R0 and k for files
+
+        
+        r0List = []
+        kList = []
+        for i in range(trials):
+            R0, k = priors(A_R0, B_R0, A_k, B_k)
+            r0List.append(R0)
+            kList.append(k)            
+            
+            
+        paramDF = pd.DataFrame(list(zip(r0List, kList)), columns = ['R0', 'k'])
+        
+        
+        paramDF.to_csv('trials.csv')
+        trial_fpath = os.path.join(self.dirpath, 'trial.csv')
+        
+        
+        
+if __name__=='__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--num_trials', typer = int , help = 'number of simulation runs')
+    
+    
+    args = parser.parse_args()
+    
+    trials = args.trials
+    WriteFile(trials = trials)
+    
+        
+
+
+
+
