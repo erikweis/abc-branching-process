2a2233162a065b471bc6d5954ec161e2b613770c
erikweis <32458904+erikweis@users.noreply.github.com>
2021-11-30 13:58:22 -0500

    update meta submitter

--- meta_submitter_all_python.py
+++ meta_submitter_all_python.py
@@ -7,15 +7,7 @@ def submit(args):
 
     script = f'/usr/bin/sbatch'
     script += f'--export=ALL,NUM_TRIALS={args.num_trials},STATE={args.state},ERROR={args.error},FOLDERNAME={args.foldername}'
-    script += f"""#!/bin/bash
-            #SBATCH --partition=short
-            #SBATCH --nodes=1
-            #SBATCH --mem=2gb
-            #SBATCH --time=0:20:00
-            #SBATCH --job-name=1997
-            """
-    script += """python submitter_all_python.py --num_trials $NUM_TRIALS --state $STATE --error $ERROR --foldername $FOLDERNAME"""
-
+    script += 'subscript.sbatch'
     subprocess.call([script],shell=True)
 
 
